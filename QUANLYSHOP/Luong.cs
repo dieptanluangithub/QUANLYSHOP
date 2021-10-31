07499ee7f25dcfddfad068a941757f7febd641c4
Diệp Tấn Luân <dieptanluanwork@gmail.com>
2021-11-01 00:51:31 +0700

    oop

---
+++ QUANLYSHOP/Luong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QUANLYSHOP
+{
+    public interface Luong
+    {
+        public double LuongCoBan()
+        {
+            return 0;
+        }
+        public double LuongTangCa()
+        {
+            return 0;
+        }
+        public double TienThuong() 
+        {
+            return 0;
+        }
+        public double LuongTong() 
+        {
+            return 0;
+
+        }
+    }
+}
